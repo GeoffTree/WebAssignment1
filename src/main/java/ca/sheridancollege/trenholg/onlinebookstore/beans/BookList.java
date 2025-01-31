3472ea27a34b1e8ec756bd64d35a67f7008d6508
Geoffrey Trenholm <trenholg@sheridancollege.ca>
2025-01-30 21:42:45 -0500

    small changes

--- src/main/java/ca/sheridancollege/trenholg/onlinebookstore/beans/BookList.java
+++ src/main/java/ca/sheridancollege/trenholg/onlinebookstore/beans/BookList.java
@@ -1,4 +1,9 @@
 package ca.sheridancollege.trenholg.onlinebookstore.beans;
 
+import java.awt.print.Book;
+import java.util.ArrayList;
+
 public class BookList {
+    private ArrayList<Book> books;
+    public ArrayList<Book> getBooks() { return books; }
 }
--- src/main/java/ca/sheridancollege/trenholg/onlinebookstore/beans/book.java
+++ src/main/java/ca/sheridancollege/trenholg/onlinebookstore/beans/book.java
@@ -1,5 +1,10 @@
 package ca.sheridancollege.trenholg.onlinebookstore.beans;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
 public class book {
     private String bookISBN;
     private String bookTitle;
